fec05afc2d22ba9f8f1d4821fac05463c98e5a4b
MoonC12 <scdvd@naver.com>
2021-05-16 15:38:41 +0900

    Base Game

---
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2015180009/dragonflight/framework/BoxCollidable.java
@@ -0,0 +1,8 @@
+package kr.ac.kpu.game.s2015180009.dragonflight.framework;
+
+import android.graphics.RectF;
+
+public interface BoxCollidable {
+    //public RectF getBoundingRect();
+    public void getBoundingRect(RectF rect);
+}
---
+++ DragonFlight/app/src/main/java/kr/ac/kpu/game/s2015180009/dragonflight/framework/Recyclable.java
@@ -0,0 +1,5 @@
+package kr.ac.kpu.game.s2015180009.dragonflight.framework;
+
+public interface Recyclable {
+    public void recycle();
+}
